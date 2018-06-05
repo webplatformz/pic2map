3f5c71947ca04d4ea38ea6455b01de638e26a6d7
Nicolas Oeschger <nicolas.oeschger@zuehlke.com>
2018-06-05 17:34:41 +0200

    Added catch all rule

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.post('/api/workspace', (req, res) => {
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + 'client/index.html'));
 });
 
