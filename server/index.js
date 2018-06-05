ea64a43dd4159ea045c1cd82fe2ab1a56fd7ad89
Nicolas Oeschger <nicolas.oeschger@zuehlke.com>
2018-06-05 13:23:51 +0200

    Created working server which serves React app and coresponding scripts

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client')));
+
+// Put all API endpoints under '/api'
+app.get('/api/hello', (req, res) => {
+    const helloWorld = {
+        msg: 'Hello World!!'
+    };
+
+    // Return them as json
+    res.json(helloWorld);
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + 'client/index.html'));
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log(`Pic2Map listening on ${port}`);
