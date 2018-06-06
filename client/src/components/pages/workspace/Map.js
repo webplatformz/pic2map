ca9517cc3aab6b0638e876a1c018ca8cd79da584
mleimer <martin.leimer@zuehlke.com>
2018-06-06 15:40:00 +0200

    Added marker cluster with some demo data

--- client/src/components/pages/workspace/Map.js
+++ client/src/components/pages/workspace/Map.js
@@ -1,5 +1,12 @@
 import React from 'react'
+
+import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+import 'leaflet.markercluster';
+
 import './Map.css';
 
 const accessToken = 'pk.eyJ1IjoibWxlaW1lciIsImEiOiJjamkxY2t1M3owamlkM3BwaWhndGVpM2pzIn0.zUGWyylw3BKCaBRQUN2LXQ';
@@ -15,6 +22,46 @@ export class Map extends React.Component {
             "images": [{
                 "geo": {
                     "geometry": {"coordinates": [47.27, 8.50], "coordType": "Point"},
+                    "properties": {"name": "Bora Bora"},
+                    "geoType": "Feature"
+                },
+                "_id": "5b17a6acf6cbad6be4a92b17",
+                "key": "424242imgeuuid",
+                "dateIso": "2007-12-24T18:21Z",
+                "dateTicks": "1198520460"
+            }, {
+                "geo": {
+                    "geometry": {"coordinates": [47.27, 8.50], "coordType": "Point"},
+                    "properties": {"name": "Bora Bora"},
+                    "geoType": "Feature"
+                },
+                "_id": "5b17a6acf6cbad6be4a92b17",
+                "key": "424242imgeuuid",
+                "dateIso": "2007-12-24T18:21Z",
+                "dateTicks": "1198520460"
+            }, {
+                "geo": {
+                    "geometry": {"coordinates": [47.27, 8.50], "coordType": "Point"},
+                    "properties": {"name": "Bora Bora"},
+                    "geoType": "Feature"
+                },
+                "_id": "5b17a6acf6cbad6be4a92b17",
+                "key": "424242imgeuuid",
+                "dateIso": "2007-12-24T18:21Z",
+                "dateTicks": "1198520460"
+            }, {
+                "geo": {
+                    "geometry": {"coordinates": [47.271, 8.501], "coordType": "Point"},
+                    "properties": {"name": "Bora Bora"},
+                    "geoType": "Feature"
+                },
+                "_id": "5b17a6acf6cbad6be4a92b17",
+                "key": "424242imgeuuid",
+                "dateIso": "2007-12-24T18:21Z",
+                "dateTicks": "1198520460"
+            }, {
+                "geo": {
+                    "geometry": {"coordinates": [47.6, 9], "coordType": "Point"},
                     "properties": {"name": "Bora Bora"},
                     "geoType": "Feature"
                 },
@@ -73,11 +120,27 @@ export class Map extends React.Component {
             })
             .addTo(map);
 
+        const markers = L.markerClusterGroup();
         for (const image of workspace.images) {
-            const marker = L.marker(image.geo.geometry.coordinates);
-            marker.bindPopup('Image');
-            marker.addTo(imageLayerGroup);
+                 const icon = L.icon({
+                     iconUrl: 'https://leafletjs.com/examples/custom-icons/leaf-green.png',
+                     iconSize:     [38, 95], // size of the icon
+                     iconAnchor:   [22, 94],
+                     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+                 });
+
+                 const marker = L.marker(image.geo.geometry.coordinates);
+                 marker.setIcon(icon);
+                 marker.bindPopup('<img src="https://leafletjs.com/examples/custom-icons/leaf-green.png"/>');
+                 marker.on('mouseover', function (e) {
+                     this.openPopup();
+                 });
+                 marker.on('mouseout', function (e) {
+                     this.closePopup();
+                 });
+                 marker.addTo(markers);
         }
+        map.addLayer(markers)
     }
 
     render() {
