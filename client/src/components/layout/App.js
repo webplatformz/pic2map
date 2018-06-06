1c5f284510f402bfccda335324f7f1bea4e2352c
Andreas Müller <andreas_mueller@live.com>
2018-06-06 08:56:18 +0200

    Add timeline component (UI)

--- client/src/components/layout/App.js
+++ client/src/components/layout/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import Header from './Header'
 import Main from './Main'
-import FileUpload from './FileUpload';
 
 
 const App = () => (
     <div>
         <Header/>
         <Main/>
-        <div>
-            <h2> File upload </h2>
-            <FileUpload />
-        </div>
+        {/*<div>*/}
+        {/*<h2> File upload </h2>*/}
+        {/*<FileUpload />*/}
+        {/*</div>*/}
     </div>
 );
 
--- client/src/components/pages/map/Map.js
+++ client/src/components/pages/map/Map.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import ReactMapboxGl from 'react-mapbox-gl';
+import './Timeline'
+import {Timeline} from "./Timeline";
 
 const MapBox = ReactMapboxGl({
     accessToken: 'pk.eyJ1IjoibWxlaW1lciIsImEiOiJjamkxY2t1M3owamlkM3BwaWhndGVpM2pzIn0.zUGWyylw3BKCaBRQUN2LXQ'
 });
 
 const Map = () => (
-    <MapBox // eslint-disable-next-line
-        style="mapbox://styles/mapbox/streets-v8"
-        containerStyle={{width: '100vw', height: '100vh'}}
-    />
+    <div>
+        <MapBox // eslint-disable-next-line
+            style="mapbox://styles/mapbox/streets-v8"
+            containerStyle={{width: '100vw', height: '100vh'}}
+        />
+        <Timeline></Timeline>
+    </div>
 );
 
 export default Map;
